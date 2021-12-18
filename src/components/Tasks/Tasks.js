9b46b5cbf8370a18d5556408d2b3dae7bb341f5f
Bennett Smrdel <smrdelb@gmail.com>
2021-12-18 13:43:58 -0600

    put an MUI modal in the project

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -6,6 +6,8 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import ModalUnstyled from '@mui/base/ModalUnstyled';
+import * as React from 'react';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -29,9 +31,62 @@ function Tasks() {
         });
     }
 
+    const StyledModal = styled(ModalUnstyled)`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Backdrop = styled('div')`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+
+const style = {
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  p: 2,
+  px: 4,
+  pb: 3,
+};
+
+const [open, setOpen] = React.useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
+
     return (
         <>
             <h1>Chores</h1>
+            <div>
+                <button className="add-task-btn" onClick={handleOpen}>+</button>
+                <StyledModal
+                    aria-labelledby="unstyled-modal-title"
+                    aria-describedby="unstyled-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                    BackdropComponent={Backdrop}
+                >
+                    <Box sx={style}>
+                    <h2 id="unstyled-modal-title">Add a Task</h2>
+                    <form>
+                        
+                    </form>
+                    </Box>
+                </StyledModal>
+            </div>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={8}>
                     <Item>
