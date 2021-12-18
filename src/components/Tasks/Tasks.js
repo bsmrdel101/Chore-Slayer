21467dbe91e2b62018352a0da5ac5a66d2feac19
Bennett Smrdel <smrdelb@gmail.com>
2021-12-18 13:08:49 -0600

    made it so that two collumns are now shoing in the chores screen

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -2,6 +2,18 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import TaskCard from "../TaskCard/TaskCard";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 
 function Tasks() {
     const dispatch = useDispatch();
@@ -20,16 +32,29 @@ function Tasks() {
     return (
         <>
             <h1>Chores</h1>
-            <div className="task-page-container">
-                {taskReducer.map((task) => {
-                    return (
-                        <div key={task.id} className='task-list'>
-                            <TaskCard task={task}/>
-                        </div>
-                    );
-                })}
-            </div>
-        </>
+            <Grid container spacing={2} columns={16}>
+            <Grid item xs={8}>
+                <Item>
+                    <div className='task-list'>
+                        {taskReducer.map((task) => {
+                            return (
+                                <div key={task.id}>
+                                    <TaskCard task={task}/>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </Item>
+            </Grid>
+            <Grid item xs={8}>
+                <Item>
+                    <div>
+                        <p>side 2</p>
+                    </div>
+                </Item>
+            </Grid>
+            </Grid>
+        </>    
     );
 }
 
