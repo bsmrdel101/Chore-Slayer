4393b4a147e1bc13704e3c9cdbbb85dc485e1d96
Bennett Smrdel <smrdelb@gmail.com>
2022-01-01 09:23:54 -0600

    added button to conditionally render card list, in order for user to add cards

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,10 +1,11 @@
 import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DisplayCard from "../DisplayCard/DisplayCard";
 
 function Deck() {
     const deck = useSelector((store) => store.deckReducer);
+    let [addCard, setAddCard] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -14,11 +15,24 @@ function Deck() {
         });
     }, [])
 
+    const handleAddCard = () => {
+        // Toggles state of add card to show cards
+        addCard === false ? setAddCard(true) : setAddCard(false);
+        // Conditionally renders the cards if addCard is true
+        if(addCard === false) {
+            window.scrollTo(2000, 2000);    
+        }
+    }
+
     return (
         <>
             <div className="deck-header">
                 <h2>{deck.length} / 15 cards</h2>
-                <Button variant="contained" color="success">Add Cards</Button>
+                {
+                    addCard === false ? <Button variant="contained" color="success" onClick={handleAddCard}>Add Cards</Button> 
+                :
+                    <Button variant="contained" color="error" onClick={handleAddCard}>Hide Cards</Button>
+                }
             </div>
             <section className="deck-gallery">
                 {deck.map((card_id) => {
