cb2181f97dc6a83aa933232c79ed73666fc0f69b
Bennett Smrdel <smrdelb@gmail.com>
2022-01-07 10:44:52 -0600

    added filler info and made the background fill the entire screen height

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -11,11 +11,19 @@ function Stats() {
             <div className="stats-container">
                 <div className="stat-column">
                     <h3>General Stats</h3>
-                    <p>{}</p>
+                    <p>Games Won: {}</p>
+                    <p>Games Lost: {}</p>
+                    <p>Total Games: {}</p>
                 </div>
                 <div className="stat-column">
                 <h3>Other Stats</h3>
-                    <p>{}</p>
+                    <p>Cards Played: {}</p>
+                    <p>Total Damage: {}</p>
+                    <p>Total Block: {}</p>
+                    <p>Minions Slain: {}</p>
+                    <p>Times Surrendered: {}</p>
+                    <p>Highest threat in game: {}</p>
+                    <p>Highest block in game: {}</p>
                 </div>
             </div>
         </>
