6eedb8a1182a682ef475faeb33d2e8e6d8f0f526
Bennett Smrdel <smrdelb@gmail.com>
2022-01-12 12:10:03 -0600

    finished the help block for the chore list, and added dummy buttons on the inside

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,32 +1,39 @@
+import { Button } from "@mui/material";
 
 function Help() {
     return (
         <>
             <h1 className="chore-list-title">Help</h1>
-            <section className="box-left">
-                <h2>Chore List</h2>
-                <p>stuff and things</p>
-            </section>
+            <div className="box-left">
+                <h1>Chore List</h1>
+                <ul className="box-list">
+                    <li>Click the <Button variant="contained" id="help-add-task-btn">+</Button> button to add a task</li>
+                    <li>Press the <button className="complete-btn">Complete</button> button to finish a task and add it to your history</li>
+                    <li>You can see your task history by clicking on the <button className="complete-btn">See History</button> button</li>
+                    <li>When you complete 3 tasks, you earn a random amount of coins between 50 and 250</li>
+                    <li>Coins can be spent in the deck page to get more cards for your deck</li>
+                </ul>
+            </div>
             <br />
-            <section className="box-right">
-                <h2>Deck</h2>
+            <div className="box-right">
+                <h1>Deck</h1>
                 <p>stuff and things</p>
-            </section>
+            </div>
             <br />
-            <section className="box-left">
-                <h2>Game Overview</h2>
+            <div className="box-left">
+                <h1>Game Overview</h1>
                 <p>stuff and things</p>
-            </section>
+            </div>
             <br />
-            <section className="box-right">
-                <h2>Card Types</h2>
+            <div className="box-right">
+                <h1>Card Types</h1>
                 <p>stuff and things</p>
-            </section>
+            </div>
             <br />
-            <section className="box-left">
-                <h2>Card Overview</h2>
+            <div className="box-left">
+                <h1>Card Overview</h1>
                 <p>stuff and things</p>
-            </section>
+            </div>
         </>
     );
 }
